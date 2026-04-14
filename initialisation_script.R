a4bca7050d8f4123b5d266a4cfc9d6945c1071ae
KaiGensitz <kai.gensitz@unibe.ch>
2026-04-14 14:40:05 +0200

    Final assessment report for Projects in R course.

--- initialisation_script.R
+++ initialisation_script.R
@@ -1,13 +1,7 @@
-# ------------------------------------------------------------
-# Nutzung der unibeCols Farbpalette (Universität Bern) in R
-# ------------------------------------------------------------
-# 1) Installation des Pakets von GitHub (nur einmal nötig):
+# Use unibeCols colours (University of Bern) in R
 # install.packages("remotes")
 # remotes::install_github("CTU-Bern/unibeCols")
 #file:///C:/Users/gensitz/OneDrive%20-%20Universitaet%20Bern/Desktop/Promotion/Kurse/Public%20Health/Projects%20in%20R/2026_course_slides_02_dataviz.html#/unibecols
-# 2) Paket laden:
-library(unibeCols)
-
 
 # Install packages if they aren't already installed
 if (!("usethis" %in% installed.packages())) install.packages("usethis")
@@ -19,7 +13,6 @@ if (!("cowplot" %in% installed.packages())) install.packages("cowplot")
 if (!("forcats" %in% installed.packages())) install.packages("forcats")
 if (!("stringr" %in% installed.packages())) install.packages("stringr")
 if (!("lubridate" %in% installed.packages())) install.packages("lubridate")
-if (!("unibeCols" %in% installed.packages())) install.packages("unibeCols")
 
 # Load packages 
 library(usethis)
@@ -32,6 +25,7 @@ library(forcats)
 library(stringr)
 library(lubridate)
 library(ggplot2)
+library(unibeCols)
 
 # Initialize Git
 
